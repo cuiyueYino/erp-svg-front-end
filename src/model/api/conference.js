ecca5779f07af8047477b846728ea6f991a5fa73
Ayton-jio <1206662817@qq.com>
2020-07-31 08:12:22 +0800

    组织架构和人员架构

--- src/model/api/conference.js
+++ src/model/api/conference.js
@@ -3,13 +3,14 @@ import httpReqest from '../../utils/https';
 const v = base.dev;
 //const testV = base.test;
 const confMangement = {
+    //================================会议室档案==============================================
     // 获取会议室列表数据
-    getConfOfficeList(params){
+    getConfOfficeList(params) {
         return httpReqest.post('/api/interfaces/confOffice/findConfOfficeList', params);
     },
 
     // 新增或者修改会议室
-    submitConfOffice(params){
+    submitConfOffice(params) {
         return httpReqest.post('/api/interfaces/confOffice/submitConfInfo', params);
     },
 
@@ -20,7 +21,7 @@ const confMangement = {
 
     // 删除会议室
     deleteConfOffice(params) {
-        return httpReqest.get('/api/interfaces/confOffice/deleteConfOffice?foid='+ params);
+        return httpReqest.get('/api/interfaces/confOffice/deleteConfOffice?foid=' + params);
     },
 
     // 获取有效会议室列表
@@ -30,9 +31,35 @@ const confMangement = {
 
     // 根据foid获取会议室
     getConfOfficeById(params) {
-        return httpReqest.get('/api/interfaces/confOffice/findConfOfficeById?foid='+ params);
+        return httpReqest.get('/api/interfaces/confOffice/findConfOfficeById?foid=' + params);
+    },
+    //================================会议申请==============================================
+    // 获取会议申请列表
+    getApplyList(params) {
+        return httpReqest.post('/api/interfaces/confApply/findApplyList', params);
     },
 
+    // 暂存会议申请
+    stagingConfApply(params) {
+        return httpReqest.post('/api/interfaces/confApply/stagingConfApply', params);
+    },
+
+    // 提交会议申请
+    submitConfApply(params) {
+        return httpReqest.post('/api/interfaces/confApply/submitConfApply', params);
+    },
+
+    // 取消会议申请
+    cancelConfApply(params) {
+        return httpReqest.get('/api/interfaces/confApply/cancelConfApply?foid=' + params);
+    },
+
+    // 根据foid获取会议室
+    getApplyById(params) {
+        return httpReqest.get('/api/interfaces/confApply/findApplyById?foid=' + params);
+    },
+
+    //================================会议室使用详情==============================================
     // 获取会议室使用详情列表数据
     getConfUsageDetail(params) {
         return httpReqest.post('/api/interfaces/confOffice/findConfUsageDetail', params);
@@ -41,7 +68,30 @@ const confMangement = {
     // 会议室使用详情Excel导出
     confUsageDetailExport(params) {
         return httpReqest.post('/api/interfaces/confOffice/confUsageDetailExport', params);
-    }
+    },
+
+    //================================组织架构==============================================
+    //  人员-根据ID查找人员
+    getStaffInfoById(params){
+        return httpReqest.post('/api/interfaces/staffManage/findStaffById?id=' + params);
+    },
+
+    // 获取人员树结构
+    getStaffTreeList(params) {
+        return httpReqest.post('/api/interfaces/staffManage/getStaffTree', params);
+    },
+
+    //公司部门人员树
+    getOrgunitTree(params){
+        var valueS='?';
+        for(var item in params){
+            valueS+=item+"="+params[item]+"&";
+        }
+        if(valueS.slice(valueS.length-1,valueS.length) ==="&"){
+            valueS=valueS.slice(0,valueS.length-1);
+        }
+        return httpReqest.get('/api/wfInterfaces/workFlow/findCompanyDeptStaffInfoByOrgUnitIdFromRedis'+valueS);
+    },
 };
 
 export default confMangement;
---
+++ src/views/conference/staff-tree-search.vue
@@ -0,0 +1,260 @@
+<template>
+  <div>
+    <el-dialog
+      :title="title"
+      :visible.sync="dialogVisible"
+      v-if="dialogVisible"
+      :modal="false"
+      :close-on-click-modal="closeConfig"
+      :close-on-press-escape="closeConfig"
+      @close="closeDialog"
+      style="z-index:2007"
+    >
+      <el-card class="box-card" v-if="!strictly">
+        <el-row :gutter="14">
+          <el-col :span="22">
+            <el-form :inline="true" :model="form" ref="form" class="demo-form-inline">
+              <el-form-item prop="selectVal">
+                <el-input
+                  class="selectVal"
+                  size="mini"
+                  clearable
+                  v-model="form.selectVal"
+                  placeholder="请输入员工姓名"
+                ></el-input>
+              </el-form-item>
+              <el-form-item>
+                <el-button type="primary" size="mini" plain @click="searchKey">搜索</el-button>
+              </el-form-item>
+              <el-form-item>
+                <el-button size="small" @click="saveConfig">确 定</el-button>
+              </el-form-item>
+            </el-form>
+          </el-col>
+        </el-row>
+      </el-card>
+      <el-form>
+        <div style="height:350px;overflow:auto">
+          <el-tree
+            :data="treeData"
+            ref="tree"
+            node-key="foid"
+            :props="defaultProps"
+            :check-strictly="strictly"
+            show-checkbox
+            check-on-click-node
+            @check-change="handleNodeClick"
+          ></el-tree>
+        </div>
+        <el-row :gutter="20"></el-row>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button size="small" @click="saveConfig" v-if="strictly">确 定</el-button>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "staffTreeSearch",
+  components: {},
+  props: {
+    type: {
+      type: String,
+      default: "",
+    },
+    companyId: {
+      type: String,
+      default: "_DefaultCompanyOId",
+    },
+    // 当前配置节点类型
+    title: {
+      type: String,
+      default: "",
+    },
+    // 配置对话框显示标识
+    visible: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      form: {
+        selectVal: "",
+      },
+      // 关闭对话框配置
+      closeConfig: false,
+      // 对话框显示标识
+      dialogVisible: this.visible,
+      treeData: [],
+      strictly: false,
+      defaultProps: {
+        label: "fname",
+        children: "children",
+      },
+    };
+  },
+  computed: {},
+  watch: {
+    // 对话框显示 自动聚焦name输入框
+    visible(bool) {
+      this.dialogVisible = bool;
+      this.treeData = [];
+      if (this.title == "用户查询") {
+        this.strictly = false;
+        let fromdata = {};
+        fromdata.id = this.companyId;
+        this.$api.confMangement.getStaffTreeList(fromdata).then(
+          (res) => {
+            let resData = res.data.data;
+            let resDataArr = eval("(" + resData + ")");
+            this.treeData = resDataArr;
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
+      } else if (this.title == "组织机构查询") {
+        this.strictly = true;
+        let fromdata = {};
+        fromdata.orgUnitId = this.companyId;
+        fromdata.queryType = "org";
+        this.$api.confMangement.getOrgunitTree(fromdata).then(
+          (res) => {
+            let resData = res.data.data;
+            let resDataArr = eval("(" + resData + ")");
+            this.treeData = resDataArr;
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
+      }
+    },
+  },
+  methods: {
+    // 查询
+    searchKey() {
+      if (this.title == "用户查询") {
+        let fromdata = {};
+        fromdata.id = this.companyId;
+        fromdata.name = this.form.selectVal;
+        this.$api.confMangement.getStaffTreeList(fromdata).then(
+          (res) => {
+            let resData = res.data.data;
+            let resDataArr = eval("(" + resData + ")");
+            this.treeData = resDataArr;
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
+      } else if (this.title == "组织机构查询") {
+        let fromdata = {};
+        fromdata.orgUnitId = this.companyId;
+        fromdata.queryType = "org";
+        this.$api.confMangement.getOrgunitTree(fromdata).then(
+          (res) => {
+            let resData = res.data.data;
+            let resDataArr = eval("(" + resData + ")");
+            this.treeData = resDataArr;
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
+      }
+    },
+    // 保存
+    saveConfig() {
+      let selectData = null;
+      if (this.title == "用户查询") {
+        selectData = this.$refs.tree.getCheckedNodes(true, true);
+      } else {
+        selectData = this.$refs.tree.getCheckedNodes();
+      }
+      console.log(selectData);
+      if (selectData.length > 0) {
+        if (selectData.length > 1) {
+          this.$message.error("只能选择一条数据!");
+        } else {
+          let dialogType = this.title;
+          let typeS = this.type;
+          if (this.title == "用户查询") {
+            // 获取员工信息
+            this.$api.confMangement
+              .getStaffInfoById(selectData[0].foid)
+              .then((res) => {
+                if (res.data.code == 0) {
+                  let serchData = {
+                    foid: res.data.data.toid,
+                    fname: res.data.data.tname,
+                    fdeptid: res.data.data.tdepartmentoid,
+                    fdeptname: res.data.data.tdepartmentname,
+                  };
+                  this.$emit("closeDialog", serchData, dialogType, typeS);
+                } else {
+                  this.$message.error(res.data.msg);
+                }
+              }),
+              (error) => {
+                console.log(error);
+              };
+          } else if (this.title == "组织机构查询") {
+            let serchData = {
+              foid: selectData[0].foid,
+              fname: selectData[0].fname,
+            };
+            this.$emit("closeDialog", serchData, dialogType, typeS);
+          }
+        }
+      } else {
+        this.$message.error("请选择一行数据!");
+      }
+    },
+    handleNodeClick(data, checked) {
+      if (checked) {
+        this.$refs.tree.setCheckedNodes([data]);
+        if (this.$refs.tree.getCheckedNodes(true).length > 1) {
+          this.goOut("清单选");
+        }
+      }
+    },
+    // 关闭模态框时子组件传值给父组件
+    closeDialog() {
+      let serchData = [];
+      let title = this.title;
+      let typeS = this.type;
+      this.$emit("closeDialog", serchData, title, typeS);
+    },
+  },
+};
+</script>
+<style lang="less" scoped>
+/deep/ .selectVal .el-input__inner {
+  width: 100%;
+}
+
+/deep/ .el-card__body {
+  padding: 6px !important;
+}
+
+/deep/ .el-dialog {
+  width: 30% !important;
+}
+
+/deep/ .el-dialog__header {
+  padding: 15px 20px 0px !important;
+}
+
+/deep/ .el-dialog__body {
+  padding: 12px 20px !important;
+}
+
+.box-card:first-child {
+  margin-bottom: 16px;
+  height: 53px;
+}
+</style>
