241cbdb7efd83cfd984deeeea4d92a4e4d15764d
sly1234567 <1126439235@qq.com>
2020-08-08 14:25:11 +0800

    新的页面增加

--- src/views/plan-options/each-person-each-table-report.vue
+++ src/views/plan-options/each-person-each-table-report.vue
@@ -1,19 +1,18 @@
 <template>
     <div v-if="ShowFinancVisible" class="itemDivStyle">
-    <div>一人一表任务表（报备）开始。。。。。。。。。。</div>
+    <div>一人一表任务表（报备功能）开始。。。。。。。。。。</div>
         <el-form
             label-width="110px"
             v-model="formdata"
             class="dataForm"
-            :rules="rules"
             size="mini"
             :model="formdata"
             :label-position="labelPosition"
         >
             <el-row>
-                <el-col :span="12">
-                    <el-form-item label="公司" prop="company">
-                        <el-select v-model="formdata.company" value-key="value" v-bind:disabled="disabled">
+                <el-col :span="6">
+                    <el-form-item label="公司：">
+                        <el-select v-model="formdata.company" value-key="value" :disabled="true">
                             <el-option
                                 v-for="item in companyData"
                                 :key="item.value"
@@ -23,9 +22,9 @@
                         </el-select>
                     </el-form-item>
                 </el-col>
-                <el-col :span="12">
-                    <el-form-item label="项目">
-                        <el-select v-model="formdata.project" value-key="value" v-bind:disabled="disabled">
+                <el-col :span="6" :offset="2">
+                    <el-form-item label="项目：">
+                        <el-select v-model="formdata.project" value-key="value" :disabled="true">
                             <el-option
                                 v-for="item in companyData"
                                 :key="item.value"
@@ -38,139 +37,137 @@
             </el-row>
             <el-row>
                 <el-col :span="6">
-                    <el-form-item label="部门">
-                        <el-input v-model="formdata.bumen" v-bind:disabled="disabled"></el-input>
+                    <el-form-item label="部门：">
+                        <el-input v-model="formdata.bumen" disabled="true"></el-input>
                     </el-form-item>
                 </el-col>
                 <el-col :span="6" :offset="2">
-                    <el-form-item label="任务状态">
-                        <el-input v-model="formdata.renwuzhaungtai" v-bind:disabled="disabled"></el-input>
+                    <el-form-item label="任务状态：">
+                        <el-input v-model="formdata.renwuzhaungtai" disabled="true"></el-input>
                     </el-form-item>
                 </el-col>
                 <el-col :span="6" :offset="2">
-                    <el-form-item label="任务类型">
-                        <el-input v-model="formdata.renwuleixing" v-bind:disabled="disabled"></el-input>
+                    <el-form-item label="任务类型：">
+                        <el-input v-model="formdata.renwuleixing" disabled="true"></el-input>
                     </el-form-item>
                 </el-col>
             </el-row>
             <el-row>
                 <el-col :span="6">
-                    <el-form-item label="任务级别">
-                        <el-input v-model="formdata.renwujibie" v-bind:disabled="disabled"></el-input>
+                    <el-form-item label="任务级别：">
+                        <el-input v-model="formdata.renwujibie" disabled="true"></el-input>
                     </el-form-item>
                 </el-col>
-                <el-col :span="3">
-                    <el-form-item label="周期性任务" ><el-checkbox v-model="formdata.checked"></el-checkbox></el-form-item>
+                <el-col :span="3" :offset="1">
+                    <el-form-item label="周期性任务：" ><el-checkbox v-model="formdata.checked" disabled="true"></el-checkbox></el-form-item>
                 </el-col>
-                <el-col :span="3">
-                    <el-form-item label="年计划调整" ><el-checkbox v-model="formdata.checked"></el-checkbox></el-form-item>
+                <el-col :span="2">
+                    <el-form-item label="年计划调整：" ><el-checkbox v-model="formdata.checked" disabled="true"></el-checkbox></el-form-item>
                 </el-col>
-            </el-row>   
-            <el-row>
-                <el-col :span="12">
-                    <el-form-item label="重点级别">
-                        <el-checkbox-group v-model="focusLevelCheckList">
+                <el-col :span="9" :offset="2">
+                    <el-form-item label="重点级别：">
+                        <el-checkbox-group v-model="focusLevelCheckList" disabled="true">
                             <el-checkbox label="集团重点"></el-checkbox>
                             <el-checkbox label="公司重点"></el-checkbox>
                             <el-checkbox label="部门重点" disabled></el-checkbox>
                         </el-checkbox-group>
                     </el-form-item>
-                </el-col>
+                </el-col>  
             </el-row>
             <el-row>
                 <el-col :span="6">
-                    <el-form-item label="工作名称">
-                        <el-input v-model="formdata.gongzuomingcheng" v-bind:disabled="disabled"></el-input>
+                    <el-form-item label="工作名称：">
+                        <el-input v-model="formdata.gongzuomingcheng" :disabled="true"></el-input>
                     </el-form-item>
                 </el-col>
-                <el-col :span="18">
-                    <el-form-item label="工作标准">
-                        <el-input v-model="formdata.gongzuobiaozhun" v-bind:disabled="disabled"></el-input>
+                <el-col :span="14" :offset="2">
+                    <el-form-item label="工作标准：">
+                        <el-input v-model="formdata.gongzuobiaozhun" :disabled="true"></el-input>
                     </el-form-item>
                 </el-col>
             </el-row>  
             <el-row>
                 <el-col :span="6">
-                    <el-form-item label="责任人">
-                        <el-input v-model="formdata.bumen" v-bind:disabled="disabled"></el-input>
+                    <el-form-item label="责任人：">
+                        <el-input v-model="formdata.bumen" :disabled="true"></el-input>
                     </el-form-item>
                 </el-col>
                 <el-col :span="6" :offset="2">
-                    <el-form-item label="检查人">
-                        <el-input v-model="formdata.renwuzhaungtai" v-bind:disabled="disabled"></el-input>
+                    <el-form-item label="检查人：">
+                        <el-input v-model="formdata.renwuzhaungtai" :disabled="true"></el-input>
                     </el-form-item>
                 </el-col>
                 <el-col :span="6" :offset="2">
-                    <el-form-item label="交办人">
-                        <el-input v-model="formdata.renwuleixing" v-bind:disabled="disabled"></el-input>
+                    <el-form-item label="交办人：">
+                        <el-input v-model="formdata.renwuleixing" :disabled="true"></el-input>
                     </el-form-item>
                 </el-col>
             </el-row>
             <el-row>
                 <el-col :span="6">
-                    <el-form-item label="开始时间">
-                        <el-input v-model="formdata.starttime" v-bind:disabled="disabled"></el-input>
+                    <el-form-item label="开始时间：">
+                        <el-input v-model="formdata.starttime" :disabled="true"></el-input>
                     </el-form-item>
                 </el-col>
                 <el-col :span="6" :offset="2">
-                    <el-form-item label="完成时间">
-                        <el-input v-model="formdata.endtime" v-bind:disabled="disabled"></el-input>
+                    <el-form-item label="完成时间：">
+                        <el-input v-model="formdata.endtime" :disabled="true"></el-input>
                     </el-form-item>
                 </el-col>
                 <el-col :span="6" :offset="2">
-                    <el-form-item label="秘书">
-                        <el-input v-model="formdata.mishu" v-bind:disabled="disabled"></el-input>
+                    <el-form-item label="秘书：">
+                        <el-input v-model="formdata.mishu" :disabled="true"></el-input>
                     </el-form-item>
                 </el-col>
             </el-row>
             <el-row>
-                <el-col :span="6" prop="mame">
-                    <el-form-item label="完成情况(%)"> 
-                        <el-input v-model="formdata.text1" v-bind:disabled="disabled"></el-input>
+                <el-col :span="6">
+                     <el-form-item label="完成情况(%)：">
+                        <el-input v-model="formdata.text1" :disabled="true"></el-input>
                     </el-form-item>
                 </el-col>
                 <el-col :span="6" :offset="2">
-                    <el-form-item label="经办人">
-                        <el-input v-model="formdata.text1" v-bind:disabled="disabled"></el-input>
+                    <el-form-item label="经办人：">
+                        <el-input v-model="formdata.text1" :disabled="true"></el-input>
                     </el-form-item>
                 </el-col>
                 <el-col :span="6" :offset="2">
-                    <el-form-item label="经办时间">
-                        <el-input v-model="formdata.text1" v-bind:disabled="disabled"></el-input>
+                    <el-form-item label="经办时间：">
+                        <el-input v-model="formdata.text1" :disabled="true"></el-input>
                     </el-form-item>
                 </el-col>
             </el-row>
             <el-row>
                 <el-col :span="6">
-                    <el-form-item label="是否自己完成" ><el-checkbox v-model="formdata.checked"></el-checkbox></el-form-item>
+                    <el-form-item label="是否自己完成："><el-checkbox v-model="formdata.checked" :disabled="true"></el-checkbox></el-form-item>
                 </el-col>
                 <el-col :span="5" :offset="2">
-                    <el-form-item label="协办人">
-                        <el-input v-model="formdata.text1" v-bind:disabled="disabled"></el-input>
+                    <el-form-item label="协办人：">
+                        <el-input v-model="formdata.text1" :disabled="true"></el-input>
                     </el-form-item>
                 </el-col>
                 <el-col :span="1">
-                    <el-button type="primary" size="mini" icon="el-icon-search" @click=""></el-button>  
+                    <el-button type="primary" size="mini" icon="el-icon-search" :disabled="true"></el-button>
                 </el-col>
             </el-row>
-             <el-row>
-                <el-col :span="22" :offset="2">
-                    <el-form-item label="情况说明">
-                        <el-input type="textarea" v-model="formdata.text" :rows="5"></el-input>
+            <el-row>
+                <el-col :span="22">
+                    <el-form-item label="情况说明：">
+                        <el-input v-model="formdata.text1" :disabled="true"></el-input>
                     </el-form-item>
                 </el-col>
             </el-row>
             <el-row>
-                <el-col :span="22" :offset="2">
-                    <el-form-item label="备注">
-                        <el-input type="textarea" v-model="formdata.beizhu" :rows="5"></el-input>
+                <el-col :span="22">
+                    <el-form-item label="备注：">
+                        <el-input v-model="formdata.text1" :disabled="true"></el-input>
                     </el-form-item>
                 </el-col>
             </el-row>
             <el-tabs v-model="atctiveName" @tab-click="handleClick">
-                <el-tab-pane label="附件" name="second">
+                <el-tab-pane label="附件" name="first">
                     <dynamic-table
-                        :columns="attachmentColumns"
+                        :columns="attachColumns"
                         :table-data="tableData"
                         :total="total"
                         size="mini"
@@ -182,10 +179,9 @@
                         element-loading-text="加载中"
                     ></dynamic-table>
                 </el-tab-pane>
-            </el-tabs> 
-            
+            </el-tabs>
         </el-form>
-    <div>一人一表任务表（报备）结束。。。。。。。。</div>
+    <div>一人一表任务表（报备功能）结束。。。。。。。。</div>
     </div>
     
 </template>
@@ -194,14 +190,29 @@ import proData from '../../components/common/proData/proData';
 import DynamicTable from '../../components/common/dytable/dytable.vue';
 export default {
     props: {
-        rowEACHPerEachJobDataObj: Object,
-        rowEACHPerEachJobtype:Boolean,
+        rowEachPerEachTableDelayDataObj: Object,
+        rowEachPerEachTableReporttype:Boolean,
     },
     components: {
         DynamicTable,
     },
     data(){
         return{
+            attachColumns:[
+                {
+                    key: 'key1',
+                    title: '序号'
+                },
+                {
+                    key: 'key2',
+                    title: '名称'
+                },
+                {
+                    key: 'key3',
+                    title: '操作'
+                },
+            ],
+            formLabelWidth: '120px',
             ShowFinancVisible:false,
             peopleJobgsTableVisible: false,
             disabled:false,
@@ -223,26 +234,7 @@ export default {
                 kaopingfangfa:'',
                 beizhu:''
             },
-            attachmentColumns:[
-                {
-                    key: 'key1',
-                    title: '序号'
-                },
-                {
-                    key: 'key2',
-                    title: '名称'
-                },
-                {
-                    key: 'key3',
-                    title: '操作'
-                },
-            ],
             tableData:[],
-            rules: {
-                name:[{ required: true, message: '请输入名称', trigger: 'blur' }],
-                company:[{ required: true, message: '请选择公司', trigger: 'blur' }],
-                code:[{ required: true, message: '请输入编码', trigger: 'blur' }],
-            },
             peopleTableRules:{
                 zhibiaoku: [
                 { required: true, message: "请输入xxxxxx", trigger: "blur" }
@@ -257,6 +249,9 @@ export default {
         }
     },
     methods: {
+        handleClick(){
+
+        },
         //pop框里面的新增表格行
         baseInputTable(Str) {
             alert("新增111111111");
@@ -265,9 +260,6 @@ export default {
         MoreSearchPS(){
 
         },
-        handleClick(){
-
-        }
     },
     watch:{
         //新建一岗一表行数据清空
@@ -282,8 +274,8 @@ export default {
                 break;
             }
             },
-        rowEACHPerEachJobtype(oldVal,newVal){
-            this.ShowFinancVisible=this.rowEACHPerEachJobtype;
+        rowEachPerEachTableReporttype(oldVal,newVal){
+            this.ShowFinancVisible=this.rowEachPerEachTableReporttype;
         }
     }
 }
