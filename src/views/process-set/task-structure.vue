8b2a6f065c73e62dd103bfb845ae2eabaed4cb21
sly1234567 <1126439235@qq.com>
2020-08-17 09:30:13 +0800

    外部路由增加

---
+++ src/views/process-set/task-structure.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="most-warapper" style="width:100%;height:600px;">     
+       <iframe src="http://192.168.85.96:6013/taskStructure" style="width:100%;height:600px;border:0px;padding:0px;"  class="test1111"></iframe>
+    </div>
+</template>
+
+7 <script>
+export default {
+     data () {
+        return {
+            
+        }
+    },
+    mounted () {
+       
+    },
+}
+23 </script>
+<style>
+    .header {
+        z-index:999;
+    }
+    .sidebar {
+        z-index:999;
+    }
+    .test1111 {
+            margin-top: -41px;
+    z-index: 0;
+    position: absolute;
+    }
+    .tags {
+        z-index:999;
+    }
+</style>
