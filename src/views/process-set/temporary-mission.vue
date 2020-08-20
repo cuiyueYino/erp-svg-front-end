ea12054d573ace2ba0d9017135df91a90ba93b7c
sly1234567 <1126439235@qq.com>
2020-08-20 15:35:48 +0800

    新

--- src/views/process-set/temporary-mission.vue
+++ src/views/process-set/temporary-mission.vue
@@ -1,4 +1,5 @@
 <template>
+    
     <div class="most-warapper">     
        <iframe src="http://192.168.85.96:6013/temporaryMission" style="width:100%;height:764px;border:0px;padding:0px;"  class="temporaryMission"></iframe>
     </div>
