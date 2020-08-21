2e3d88d6f028fad90c6abdb019a46c1bbf2d5831
孟鹏飞 <437994940@qq.com>
2020-08-21 17:57:46 +0800

    修改bug

--- src/permission.js
+++ src/permission.js
@@ -4,13 +4,18 @@ import router from './router'
 
 // 使用钩子函数对路由进行权限跳转
 router.beforeEach((to, from, next) => {
+	console.log(to)
 	const token = localStorage.getItem('ms_tokenId');
 	if(token) {
 		//跳转页面的url是否存在
-		if(to.path.indexOf("none") == "-1") {
-			next()
+		if(to.path == "/login") {
+			next('/')
 		} else {
-			next("/erp/404")
+			if(to.path.indexOf("none") == "-1") {
+				next()
+			} else {
+				next("/erp/404")
+			}
 		}
 	} else {
 		if(to.name == "login" || to.name == "confMntLogin") {
