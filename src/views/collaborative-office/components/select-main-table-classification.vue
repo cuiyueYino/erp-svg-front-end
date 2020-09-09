b4f4206ef1f769240912cea89a6374ddcb4a08ef
sunshuang <shuangvirus@163.com>
2020-09-09 18:29:09 +0800

    bug 324

--- src/views/collaborative-office/components/select-main-table-classification.vue
+++ src/views/collaborative-office/components/select-main-table-classification.vue
@@ -206,14 +206,11 @@
 			},
 			//搜索
 			toSelect() {
+				if (this.show == 1){
+					this.formInline.status = 3;
+				}
 				this.$api.collaborativeOffice.findWorkItemTypePage(this.formInline).then(data => {
-						let resultList =  data.data.data.rows;
-						resultList.forEach(item => {
-							//只显示有效数据
-							// if (item.status == "3"){
-								this.tableData.push(item)
-							// }
-						});
+						this.tableData = data.data.data.rows;
 						this.currentTotal = this.tableData.total;
 				})
 			},
