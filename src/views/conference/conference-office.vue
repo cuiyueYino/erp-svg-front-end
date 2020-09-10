cbd8be8cb3d8b5ee0f2f835818e504dfe96fc3b6
sunshuang <shuangvirus@163.com>
2020-09-10 16:36:01 +0800

    bug 369 441 487

--- src/views/conference/conference-office.vue
+++ src/views/conference/conference-office.vue
@@ -76,7 +76,7 @@
         <el-row :gutter="24">
           <el-col :span="12">
             <el-form-item label="公司：" :label-width="formLabelWidth" class="pop-select" prop="fcompany">
-              <el-select v-model="searchForm.fcompany" size="small"  @change="selectChange(searchForm.fcompany)" clearable placeholder="请选择">
+              <el-select v-model="searchForm.fcompanyname" size="small"  @change="selectChange(searchForm.fcompany)" clearable placeholder="请选择">
                 <el-option v-for="item in options" :key="item.id" :label="item.name" :value="item.id"></el-option>
               </el-select>
             </el-form-item>
@@ -407,10 +407,10 @@
     methods: {
       //公司下拉选择
       selectChange(value){
-        debugger;
         this.options.forEach(item => {
           if(item.id == value) {
             this.$nextTick(() => {
+              this.searchForm.fcompany = item.code;
               this.searchForm.fcompanyname = item.name;
             });
           }
