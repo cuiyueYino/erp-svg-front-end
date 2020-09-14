f8c1b7d788d3bc97919567088a4b366809b20489
sly1234567 <1126439235@qq.com>
2020-09-14 16:00:37 +0800

    btn 样式修改

--- src/views/collaborative-office/components/add-work-items.vue
+++ src/views/collaborative-office/components/add-work-items.vue
@@ -13,10 +13,10 @@
 					</el-select>
 				</el-col>
 				<el-col :span="6" style="text-align: right;">
-					<el-button v-if="showFig" @click="submitForm(2)" type="success" size="mini" icon="el-icon-check">提交</el-button>
-					<el-button v-if="showFig" @click="submitForm(1)" type="success" size="mini" icon="el-icon-finished">暂存</el-button>
-					<el-button @click="selectMainTable" type="success" size="mini" icon="el-icon-s-promotion">选择模板</el-button>
-					<el-button type="danger" @click="$parent.toSelect()" size="mini" icon="el-icon-close">返回</el-button>
+					<el-button v-if="showFig" @click="submitForm(2)" type="success" size="medium" icon="el-icon-copy-document">提交</el-button>
+					<el-button v-if="showFig" @click="submitForm(1)" type="success" size="medium" icon="el-icon-folder-remove">暂存</el-button>
+					<el-button @click="selectMainTable" type="success" size="medium" icon="el-icon-s-promotion">选择模板</el-button>
+					<el-button type="danger" @click="$parent.toSelect()" size="medium" icon="el-icon-close">返回</el-button>
 				</el-col>
 			</el-row>
 			<formAndTable :files="files" dis="2" showAdd="1" ref="child" :form-data="conData"></formAndTable>
@@ -25,8 +25,8 @@
 		<el-dialog title="工作事项模板主表" top="1vh" :destroy-on-close="true" v-if="dialogVisible" center :visible.sync="dialogVisible" width="80%">
 			<selectMainTable show="1" status="3" :userId="UserId" :company="companyID" ref="childMain"></selectMainTable>
 			<div slot="footer" class="dialog-footer">
-				<el-button @click="dialogVisible = false">取 消</el-button>
-				<el-button type="primary" @click="getDialogVisible">确 定</el-button>
+				<el-button type="success"  icon="el-icon-check" size="medium" @click="getDialogVisible">确定</el-button>
+				<el-button type="warning"  icon="el-icon-close" size="medium" @click="dialogVisible = false">取消</el-button>
 			</div>
 		</el-dialog>
 	</div>
