f91dba345262d7c1ee8cd59e1acd479cb7f48754
MolangWatson <1061485393@qq.com>
2020-10-21 14:13:23 +0800

    配置

---
+++ src/views/financial-migration/page/route.js
@@ -0,0 +1,12 @@
+const AAAA = () => import('./index.vue');
+const FINANCIAL = [
+    {
+        path: 'AAAA',
+        name: 'AAAA',
+        component: AAAA,
+        meta: {
+            title: '计划任务量化统计表'
+        }
+    }
+]
+export default FINANCIAL;
