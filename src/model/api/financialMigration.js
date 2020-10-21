7fd1808ea90db6120bde097a74fac2e15ef6ef30
孟鹏飞 <437994940@qq.com>
2020-10-21 14:21:55 +0800

    Revert "c财务接口文件"
    This reverts commit 1263c7b6dbf0091b5300144da29d62573d522794.

--- src/model/api/financialMigration.js
+++
@@ -1,8 +0,0 @@
-import base from './base'; // 导入接口域名列表
-import httpReqest from '../../utils/https';
-const v = base.dev;
-//const testV = base.test;
-const financialMigration = {
-    
-}
-export default financialMigration;
